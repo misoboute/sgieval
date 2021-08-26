3a1b461ba5d01176173fc0d072e0137b55223767
Muhammad Ismail Soboute <misoboute@gmail.com>
2021-08-26 03:45:02 +0200

    Implement Rectangle2D according to specification

--- include/sgievallib/Rectangle2D.h
+++ include/sgievallib/Rectangle2D.h
@@ -1,8 +1,52 @@
 namespace sgieval
 {
 
-class Rectangle2D
+template <typename T> class Rectangle2D
 {
+  public:
+    using CoordT = T;
+
+    Rectangle2D(CoordT x, CoordT y, CoordT width, CoordT height);
+
+    bool IntersectsWith(const Rectangle2D &other);
+
+  private:
+    bool DoIntersectAlongOneCoordinate(CoordT coord1, CoordT halfLength1, CoordT coord2, CoordT halfLength2);
+
+    CoordT m_x;
+    CoordT m_y;
+    CoordT m_halfWidth;
+    CoordT m_halfHeight;
 };
+
+template <typename T>
+Rectangle2D<T>::Rectangle2D(CoordT x, CoordT y, CoordT width, CoordT height)
+    : m_x(x), m_y(y), m_halfWidth(width / 2), m_halfHeight(height / 2)
+{
+}
+
+template <typename T>
+bool Rectangle2D<T>::DoIntersectAlongOneCoordinate(CoordT coord1, CoordT halfLength1, CoordT coord2, CoordT halfLength2)
+{
+    if (coord1 < coord2)
+    {
+        return coord2 < coord1 + halfLength1 + halfLength2;
+    }
+    else if (coord1 > coord2)
+    {
+        return coord1 < coord2 + halfLength1 + halfLength2;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+template <typename T> bool Rectangle2D<T>::IntersectsWith(const Rectangle2D<T> &other)
+{
+    auto horizontal = DoIntersectAlongOneCoordinate(m_x, m_halfWidth, other.m_x, other.m_halfWidth);
+    auto vertical = DoIntersectAlongOneCoordinate(m_y, m_halfHeight, other.m_y, other.m_halfHeight);
+    return horizontal && vertical;
+}
 
 } // namespace sgieval
